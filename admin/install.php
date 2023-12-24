b233103688424d00c3d5964381442834c13383cc
androklemens <121905288+androklemens@users.noreply.github.com>
2023-12-24 23:48:22 +0700

    Add files via upload

---
+++ admin/install.php
@@ -0,0 +1,12 @@
+<?php
+//-----------Your Email
+$admin  = "magelang1337@gmail.com";
+$result = "magelang1337@gmail.com";
+
+//-----------Your ADMINname
+$name = "Admin";
+
+
+//-----------Your Password
+$pass_word = "admin12345";
+?>
